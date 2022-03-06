e1991a73100596e75eda55d9f9916551d2d20701
Earlfeddy <100390543+Earlfeddy@users.noreply.github.com>
2022-03-06 18:15:46 +0100

    Challenge4

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,14 @@
-function withdraw(amount) {}
+function withdraw(amount) {
+  const noOf100s = Math.floor(amount / 100);
+  amount = amount - noOf100s * 100;
 
-module.exports = withdraw
+  const noOf50s = Math.floor(amount / 50);
+  amount = amount - noOf50s * 50;
+
+  const noOf20s = Math.floor(amount / 20);
+  amount = amount - noOf20s * 20;
+
+  return [noOf100s, noOf50s, noOf20s];
+}
+
+module.exports = withdraw;
