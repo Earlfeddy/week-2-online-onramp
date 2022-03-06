f2b553a4b11536dbbb1f8a3d9e71ca9aa9210f85
Earlfeddy <100390543+Earlfeddy@users.noreply.github.com>
2022-03-06 18:17:03 +0100

    Challenge5

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,17 @@
-function arraySum(arr1, arr2) {}
+function arraySum(arr1, arr2) {
+  let incrIndex = 0;
 
-module.exports = arraySum
+  const sum = arr1.map((item) => {
+    const result = item + arr2[incrIndex];
+
+    if (incrIndex >= arr2.length - 1) {
+      incrIndex = 0;
+    } else {
+      incrIndex++;
+    }
+    return result;
+  });
+  return sum;
+}
+
+module.exports = arraySum;
