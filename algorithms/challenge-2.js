6e39b849b0a97e9b597c7d57f39e21b807be9c72
Earlfeddy <100390543+Earlfeddy@users.noreply.github.com>
2022-03-06 18:12:48 +0100

    challenge2

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,19 @@
-function numSwap(number) {}
+function numSwap(number) {
+  const numStr = number.toString();
+  const numArr = numStr.split("");
 
-module.exports = numSwap
+  const chunks = [];
+
+  for (let i = 0; i < numArr.length; i += 2) {
+    const newChunk = numArr.slice(i, i + 2);
+    chunks.push(newChunk);
+  }
+  const swappedArr = chunks.map((chunk) => chunk.reverse());
+
+  const joinedArray = swappedArr.flat();
+  const str = joinedArray.join("");
+
+  return Number(str);
+}
+
+module.exports = numSwap;
