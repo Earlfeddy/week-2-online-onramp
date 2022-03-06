3aa0eb991412134b27a9f8e528a4ad6d7e3e73f8
Earlfeddy <100390543+Earlfeddy@users.noreply.github.com>
2022-03-06 18:05:42 +0100

    Challenge1

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,16 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  let newArray = [];
+
+  let minimum = Math.min(...numArr);
+  let maximum = Math.max(...numArr);
+
+  for(let i = minimum; i < maximum; i++){
+    if(numArr.indexOf(i) < 0){
+      newArray.push(i);
+      return newArray;
+    }
+  }
+  return false;  
+}
 
 module.exports = missingNumber
